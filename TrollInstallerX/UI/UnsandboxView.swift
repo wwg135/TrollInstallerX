21d25aba47c2c193a5f044035f73482c03c519bb
wwg135 <105048516+wwg135@users.noreply.github.com>
2024-04-05 10:31:22 +0800

    Update UnsandboxView.swift

--- TrollInstallerX/UI/UnsandboxView.swift
+++ TrollInstallerX/UI/UnsandboxView.swift
@@ -15,7 +15,7 @@ struct UnsandboxView: View {
                     .font(.system(size: 23, weight: .semibold, design: .rounded))
                     .foregroundColor(.white)
                     .padding()
-                Text(TrollInstallerX 使用100%可靠的 MacDirtyCow 漏洞来解除沙盒并复制内核缓存。按下下方的按钮运行该漏洞利用程序-您只需要这样操作一次。)
+                Text("TrollInstallerX 使用100%可靠的 MacDirtyCow 漏洞来解除沙盒并复制内核缓存。按下下方的按钮运行该漏洞利用程序-您只需要这样操作一次。")
                     .font(.system(size: 16, weight: .regular, design: .rounded))
                     .multilineTextAlignment(.center)
                     .foregroundColor(.white)
@@ -23,7 +23,7 @@ struct UnsandboxView: View {
                     UIImpactFeedbackGenerator().impactOccurred()
                     grant_full_disk_access({ error in
                         if let error = error {
-                            Logger.log("Failed to exploit with MacDirtyCow!")
+                            Logger.log("利用 MacDirtyCow 漏洞失败")
                             NSLog("Failed to MacDirtyCow - \(error.localizedDescription)")
                         }
                         withAnimation {
@@ -36,7 +36,7 @@ struct UnsandboxView: View {
                             .frame(width: 175, height: 45)
                             .foregroundColor(.white.opacity(0.2))
                             .shadow(radius: 10)
-                        Text("Unsandbox")
+                        Text("解除沙盒")
                             .font(.system(size: 20, weight: .semibold, design: .rounded))
                             .foregroundColor(.white)
                             .padding()
