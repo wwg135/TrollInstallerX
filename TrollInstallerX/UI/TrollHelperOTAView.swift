f368ac96fe6ad899a99236cadef039ba4b0248e4
wwg135 <105048516+wwg135@users.noreply.github.com>
2024-04-05 09:24:48 +0800

    Update TrollHelperOTAView.swift

--- TrollInstallerX/UI/TrollHelperOTAView.swift
+++ TrollInstallerX/UI/TrollHelperOTAView.swift
@@ -15,7 +15,7 @@ struct TrollHelperOTAView: View {
                     .font(.system(size: 23, weight: .semibold, design: .rounded))
                     .foregroundColor(.white)
                     .padding()
-                Text("Your device is compatible with TrollHelperOTA - a 100% reliable installation method that does not require you to sideload an app. You can tap outside this alert to dismiss this, or press the button below to install via OTA.")
+                Text(您的设备兼容TrollHelperOTA - 这是一种100%可靠的安装方法，无需通过侧载应用程序进行安装。您可以点击此提示之外的区域来关闭，或者按下下方的按钮通过OTA进行安装。)
                     .font(.system(size: 16, weight: .regular, design: .rounded))
                     .multilineTextAlignment(.center)
                     .foregroundColor(.white)
@@ -28,7 +28,7 @@ struct TrollHelperOTAView: View {
                             .frame(width: 175, height: 45)
                             .foregroundColor(.white.opacity(0.2))
                             .shadow(radius: 10)
-                        Text("Take me there")
+                        Text("OTA安装")
                             .font(.system(size: 16, weight: .semibold, design: .rounded))
                             .foregroundColor(.white)
                     }
